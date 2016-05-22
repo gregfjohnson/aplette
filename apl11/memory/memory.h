1c579abcb7097f2209e3e726fe3c58dc34f91a06
user <none@none>
2016-05-22 10:50:13 -0700

    replace redundant copies of .h files with symlinks.

--- apl11/memory/memory.h
+++ apl11/memory/memory.h
@@ -1,13 +1,1 @@
-/* Copyright U S WEST Advanced Technologies, Inc. (AT&T)
- * You may use, copy, modify and sublicense this Software
- * subject to the conditions expressed in the file "License".
- */
-
-struct memblock {
-	int *block;
-	unsigned nbytes;
-	struct memblock *next;
-};
-
-struct memblock *firstblock;
-extern int mem_trace;
+../include/memory.h
