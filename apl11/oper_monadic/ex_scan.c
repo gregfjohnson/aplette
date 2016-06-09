ed407496a7d3be21ef627de588979b1e03a60247
Greg Johnson <gregfjohnson@yahoo.com>
2016-06-09 14:16:59 -0700

    fix scan operator segfault on 64-bit machines

--- apl11/oper_monadic/ex_scan.c
+++ apl11/oper_monadic/ex_scan.c
@@ -9,83 +9,97 @@
 #include "data.h"
 
 void scan0(int k);
-void scan1(data *param[]);
+void scan1(data *dataPtr, data (*f)(data, data));
 
 void ex_scn0() {
-   fetch1();
-   scan0(0);
+    fetch1();
+    scan0(0);
 }
 
 void ex_scan() {
-   struct item *p;
+    struct item *p;
 
-   p = fetch1();
-   scan0(p->rank-1);
+    p = fetch1();
+    scan0(p->rank-1);
 }
 
 void ex_scnk() {
-   int i;
+    int i;
 
-   i = topfix() - iorigin;
-   scan0(i);
+    i = topfix() - iorigin;
+    scan0(i);
 }
 
 void scan0(int k) {
-   struct item *p, *q;
-   data *param[2];
+    struct item *p, *q;
+    data (*fn)(data, data);
 
-   p = fetch1();
-   if(p->type != DA) error(ERR_domain,"not numeric data");
-   bidx(p);
-   colapse(k);
-   if(idx.dimk == 0) {
-/*
- *  scan identities - ets/jrl 5/76
- */
-      q = newdat(DA,0,1);
-      q->dim[0] = 1;
-      switch(*gsip->ptr++) {
-   case ADD:
-   case SUB:
-   case OR:
-         q->datap[0] = 0;
-         break;
-   case AND:
-   case MUL:
-   case DIV:
-         q->datap[0] = 1;
-         break;
-   case MIN:
-         q->datap[0] = 1.0e38;
-         break;
-   case MAX:
-         q->datap[0] = -1.0e38;
-         break;
-   default:
-         error(ERR_implicit,"cannot scan with this function");
-      }
-      pop();
-      *sp++ = q;
-      return;
-   }
-   param[0] = p->datap;
-   param[1] = (data *)exop[*gsip->ptr++];
-   forloop(scan1, param);
+    p = fetch1();
+    if(p->type != DA) error(ERR_domain,"not numeric data");
+
+    bidx(p);
+    colapse(k);
+
+    if(idx.dimk == 0) {
+	/*
+	*  scan identities - ets/jrl 5/76
+	*/
+	q = newdat(DA,0,1);
+	q->dim[0] = 1;
+
+	switch(*gsip->ptr++) {
+	    case ADD:
+	    case SUB:
+	    case OR:
+		q->datap[0] = 0;
+		break;
+
+	    case AND:
+	    case MUL:
+	    case DIV:
+		q->datap[0] = 1;
+		break;
+
+	    case MIN:
+		q->datap[0] = 1.0e38;
+		break;
+
+	    case MAX:
+		q->datap[0] = -1.0e38;
+		break;
+
+	    default:
+		error(ERR_implicit,"cannot scan with this function");
+	}
+
+	pop();
+	*sp++ = q;
+
+	return;
+    }
+
+    fn = (data (*)(data, data)) exop[*gsip->ptr++];
+
+    indexIterateInit(&idx);
+    while (indexIterate(&idx)) {
+        scan1(p->datap, fn);
+    }
 }
 
-void scan1(data *param[]) {
-   int i, j;
-   data *dp, *ip, d, (*f)();
+void scan1(data *dataPtr, data (*f)(data, data)) {
+    int i, j;
+    data *dp, *ip, d;
 
-   f = (data (*)())param[1];
-   for (i=0; i<idx.dimk; i++) {
-      dp = param[0] + access() + (idx.dimk - (i+1)) * idx.delk;
-      ip = dp;
-      d  = *ip;
-      for (j=1; j<idx.dimk-i; j++) {
-         ip -= idx.delk;
-         d = (*f)(*ip, d);
-      }
-      *dp = d;
-   }
+    for (i=0; i<idx.dimk; i++) {
+	dp = dataPtr + access() + (idx.dimk - (i+1)) * idx.delk;
+	ip = dp;
+	d  = *ip;
+
+	for (j=1; j<idx.dimk-i; j++) {
+	    ip -= idx.delk;
+	    d = (*f)(*ip, d);
+	}
+
+	*dp = d;
+    }
 }
